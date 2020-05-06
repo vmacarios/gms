ebaf870b7d7cdcf112d11f1cc71a1154ac144276
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-05-06 13:45:30 +0200

    Spring Boot Initial Setup

--- gms-zip-service/src/main/java/com/macarios/gms/Zip.java
+++ gms-zip-service/src/main/java/com/macarios/gms/Zip.java
@@ -4,19 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author victor macarios
  * @date 2020/05
  */
 
+@SpringBootApplication
 public class Zip {
 	private static final Logger logger = LoggerFactory.getLogger(Zip.class);
 
 	public static void main(String[] args) {
-		Marker notifyAdmin = MarkerFactory.getMarker("NOTIFY_ADMIN");
+//		Marker notifyAdmin = MarkerFactory.getMarker("NOTIFY_ADMIN");
+//
+//		logger.info("The value is: {}", "info test");
+//		logger.debug(notifyAdmin, "The value is: {}", "debug test");
 
-		logger.info("The value is: {}", "info test");
-		logger.debug(notifyAdmin, "The value is: {}", "debug test");
+		SpringApplication.run(Zip.class, args);
+
 	}
 }
