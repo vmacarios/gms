10ead96f7e4af22985775571d73272475392e3d6
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-05-12 17:11:14 +0200

    GMS-8 - added save method on Service

--- gms-zip-service/src/main/java/com/macarios/gms/service/ZipService.java
+++ gms-zip-service/src/main/java/com/macarios/gms/service/ZipService.java
@@ -2,22 +2,37 @@ package com.macarios.gms.service;
 
 import com.macarios.gms.model.Zip;
 import com.macarios.gms.repository.ZipRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * This class is responsible for the interactions with the repository
+ */
 @Service
 public class ZipService {
+
 	private ZipRepository zipRepository;
 
-	public ZipService() {
-	}
-
-	public ZipService(ZipRepository zipRepository) {
+	public ZipService(@Autowired ZipRepository zipRepository) {
 		this.zipRepository = zipRepository;
 	}
 
+	/**
+	 *
+	 * @return every zip entries
+	 */
 	public List<Zip> findAll() {
 		return zipRepository.findAll();
+	}
+
+	/**
+	 *
+	 * @param zip is the object to be saved
+	 * @return the saved object
+	 */
+	public Zip save(Zip zip) {
+		return zipRepository.save(zip);
 	}
 }
