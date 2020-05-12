9c9e5984ea66705cfde76a278543eaf66026ce5a
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-05-12 16:36:50 +0200

    GMS-8 - save test

--- gms-zip-service/src/test/java/com/macarios/gms/controller/ZipControllerTest.java
+++ gms-zip-service/src/test/java/com/macarios/gms/controller/ZipControllerTest.java
@@ -1,5 +1,6 @@
 package com.macarios.gms.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.macarios.gms.model.Zip;
 import com.macarios.gms.service.ZipService;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -16,8 +18,10 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,6 +40,9 @@ public class ZipControllerTest {
 
 	@MockBean
 	private ZipService zipService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	private static List<Zip> zipList = new ArrayList<>();
 
@@ -62,7 +69,7 @@ public class ZipControllerTest {
 	}
 
 	@Test
-	public void getAllZips() throws Exception {
+	void getAllZips() throws Exception {
 		when(zipService.findAll()).thenReturn(zipList);
 		mockMvc.perform(get("/zips")
 				.contentType(MediaType.APPLICATION_JSON))
@@ -75,11 +82,37 @@ public class ZipControllerTest {
 	}
 
 	@Test
-	public void wrongUri() throws Exception {
+	void wrongUri() throws Exception {
 		mockMvc.perform(get("/zip")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound())
 				.andDo(print());
 	}
 
+	@Test
+	void successfullySavedAZip() throws Exception {
+
+		Zip zip = new Zip(
+				"15085210",
+				"R. Gago Coutinho",
+				"",
+				"Pq. Estoril",
+				"S J Rio Preto",
+				"SP",
+				Instant.now(),
+				null);
+		when(zipService.save(any(Zip.class))).thenReturn(zip);
+
+		String zipJSON = objectMapper.writeValueAsString(zip);
+
+		ResultActions result = mockMvc.perform(post("/zips")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(zipJSON)
+		);
+
+		result.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.zip").value("15085210"))
+				.andExpect(jsonPath("$.address").value("R. Gago Coutinho"));
+
+	}
 }
