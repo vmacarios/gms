adfb5b14c9364a8ea80baa5d6699931a7932962b
Victor Macarios <47126430+vmacarios@users.noreply.github.com>
2020-05-12 16:38:37 +0200

    GMS-8 - save method on controller

--- gms-zip-service/src/main/java/com/macarios/gms/controller/ZipController.java
+++ gms-zip-service/src/main/java/com/macarios/gms/controller/ZipController.java
@@ -3,9 +3,9 @@ package com.macarios.gms.controller;
 import com.macarios.gms.model.Zip;
 import com.macarios.gms.service.ZipService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,17 +13,20 @@ import java.util.List;
 public class ZipController {
 	private ZipService zipService;
 
-	public ZipController() {
-	}
-
 	@Autowired
 	public ZipController(ZipService zipService) {
 		this.zipService = zipService;
 	}
 
+
+	/**
+	 * GET method
+	 *
+	 * @return all zips on DB.
+	 */
 	@GetMapping("/zips")
 	@ResponseBody
-	List<Zip> getAllZips(){
+	List<Zip> getAllZips() {
 		return zipService.findAll();
 	}
 //	ResponseEntity<List<Zip>> getAllZips() {
@@ -31,4 +34,15 @@ public class ZipController {
 //		return new ResponseEntity<>(zipService.findAll(), HttpStatus.OK);
 //		return ResponseEntity.ok().body(zipService.findAll());
 //	}
+
+	/**
+	 * The POST method create a zip entry in DB using
+	 *
+	 * @param zip received and
+	 * @return the HttpStatus as 201 (CREATED).
+	 */
+	@PostMapping("/zips")
+	ResponseEntity<Zip> create(@RequestBody Zip zip) {
+		return new ResponseEntity<>(zipService.save(zip), HttpStatus.CREATED);
+	}
 }
